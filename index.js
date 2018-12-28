dd0437a740c2ca12eefe50fcc0a72afa20754755
AinoMegumi <aimegu2014@gmail.com>
2018-12-28 21:00:55 +0900

    create core and sever config

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const randomstring = require('crypto-random-string');
+const RoomInfo = require("./roominfo.js");
+const bodyParser = require('body-parser');
+const roomTypeValid = require('./roomTypeValidator.js');
+const app = express();
+
+/** @type {Map<RoomInfo>} */
+const roomlist = new Map();
+
+const fse = require('fs/promises');
+const jsonParser = bodyParser.json();
+
+const main = async () => {
+	const settinginfo = JSON.parse(await fse.readFile('settings.json', 'utf8'));
+	app.post('/start', jsonParser, (req, res) => {
+		if (!req.body || !roomTypeValid(req.body.gamemode)) return res.sendStatus(400);
+		let UID = randomstring(settinginfo.data.uidlength);
+		while (roomlist.has(UID)) UID = randomstring(settinginfo.data.uidlength);
+		roomlist.set(UID, new RoomInfo(req.body.gamemode));
+		setTimeout(() =>{
+			if (!roomlist.get(UID).seedIsValid()) roomlist.delete(UID);
+		}, settinginfo.data.timeout * 1000);
+		res.send(UID);
+	});
+	app.use(bodyParser.json( { type: 'application/*+json'}));
+	app.listen(3000);
+};
+
+main();
