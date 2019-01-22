b653b415fcd77b16bf28206ab7a35c09a7e8b71f
AinoMegumi <aimegu2014@gmail.com>
2019-01-22 23:01:12 +0900

    update seed validator to use spec

--- roominfo.js
+++ roominfo.js
@@ -1,12 +1,15 @@
 "use scrict";
+const s = require('@json-spec/core');
+const seedSpec = require('./spec/seed');
 
 class RoomInfo {
     constructor(gamemode){
-        this.seed_ = "";
+        this.seed_ = [];
         this.gamemode_ = gamemode;
     }
     seedIsValid(){
-        return 1250 === this.seed_.length;
+        return s.IsValid(seedSpec, this.seed_);
+    }
     }
 }
 module.exports = RoomInfo;
