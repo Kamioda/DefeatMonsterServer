ff1a11077d81c08873d59affca0de3aecdf85fd8
AinoMegumi <aimegu2014@gmail.com>
2019-01-22 23:05:42 +0900

    update to manage timer

--- roominfo.js
+++ roominfo.js
@@ -6,15 +6,22 @@ class RoomInfo {
     constructor(gamemode){
         this.seed_ = [];
         this.gamemode_ = gamemode;
+        this.timer_ = null;
     }
     setSeed(seedData) {
         this.seed_ = seedData;
+        clearTimeout(this.timer_);
+        this.timer_ = null;
     }
     seedIsValid(){
         return s.IsValid(seedSpec, this.seed_);
     }
     getSeed() {
         return this.seed_;
+    }
+    setTimer(timer) {
+        if (this.timer_ != null) clearTimeout(this.timer_);
+        this.timer_ = timer;
     }
     }
 }
