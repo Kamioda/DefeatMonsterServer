802e578a8b53a39a20dc23289cee87f484401c60
AinoMegumi <aimegu2014@gmail.com>
2019-05-05 22:04:43 +0900

    update line feed code

--- spec/seed.js
+++ spec/seed.js
@@ -1,6 +1,6 @@
 const s = require('@json-spec/core');
 
-module.exports = s.array(i => Number.isInteger(i) && i >= 0 && 4294967295 >= i, { 
+module.exports = s.array(i => Number.isInteger(i) && i >= 0 && 4294967295 >= i, {
     count : 1250,
     distinct : true
 });
