ab5bdef24f2c51d1656419ecc5c8e554ecc4c28f
AinoMegumi <aimegu2014@gmail.com>
2019-05-05 22:01:00 +0900

    add argument type info

--- roominfo.js
+++ roominfo.js
@@ -5,29 +5,46 @@ const seedSpec = require('./spec/seed');
 
 class RoomInfo {
     constructor(gamemode){
+        /** @type {number[]} */
         this.seed_ = [];
         this.gamemode_ = gamemode;
+        /** @type {NodeJS.Timeout} */
         this.timer_ = null;
+        /** @type {string[]} */
         this.player_ = [];
     }
+    /**
+     * Set seed and clear timer passed via setTimer
+     * @param {number[]} seedData
+     */
     setSeed(seedData) {
         this.seed_ = seedData;
         clearTimeout(this.timer_);
         this.timer_ = null;
     }
+    /**
+     * @returns {boolean}
+     */
     seedIsValid(){
         return s.IsValid(seedSpec, this.seed_);
     }
     getSeed() {
         return this.seed_;
     }
+    /**
+     * @param {NodeJS.Timeout} timer result of setTimeout
+     */
     setTimer(timer) {
         if (this.timer_ != null) clearTimeout(this.timer_);
         this.timer_ = timer;
     }
+    /**
+     * @param {number} pidLength length of pid from setting.json
+     * @returns {string} new pid
+     */
     addPlayer(pidLength) {
         switch (this.player_.length) {
-            case 0: 
+            case 0:
                 this.player_.push(randomstring(pidLength));
                 return this.player_[0];
             case 1: {
